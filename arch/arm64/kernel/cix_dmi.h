bd8db3e89224d32984a0b31b7b68572c1ea59145
Jianfeng Liu <Jianfeng.Liu@cixtech.com>
2026-05-25 13:45:58 +0800

    arm64: add model name for Cix Sky1 Soc

---
+++ arch/arm64/kernel/cix_dmi.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __CIX_DMI_H
+#define __CIX_DMI_H
+#include <linux/dmi.h>
+#include <asm/dmi.h>
+
+char *get_cpu_name(void);
+
+#endif /* __CIX_DMI_H */
